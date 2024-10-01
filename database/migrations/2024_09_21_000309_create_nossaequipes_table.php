82964e54aaba04351b6ca7d22b1736350f685f63
Ryan Rodrigues <ryanneno1590@gmail.com>
2024-09-30 21:50:34 -0300

    ajuste migrate nossaequipe para subir como blob tipo da foto

--- database/migrations/2024_09_21_000309_create_nossaequipes_table.php
+++ database/migrations/2024_09_21_000309_create_nossaequipes_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
 
 return new class extends Migration
 {
@@ -13,13 +15,16 @@ return new class extends Migration
     {
         Schema::create('nossaequipes', function (Blueprint $table) {
             $table->id();
-            $table->binary('foto');
+            $table->binary('foto'); // Você pode manter isso para BLOB
             $table->string('nome');
             $table->string('cargo');
             $table->string('profissao');
             $table->timestamps();
         });
+
+        DB::statement('ALTER TABLE nossaequipes MODIFY foto LONGBLOB');
     }
+
 
     /**
      * Reverse the migrations.
