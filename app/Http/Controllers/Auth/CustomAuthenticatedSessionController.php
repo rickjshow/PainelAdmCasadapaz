16918774c8f012b43347840dc16b16a0aed22854
Ryan1590 <ryanneno1590@gmail.com>
2025-04-25 12:24:11 -0300

    validação que faltou no login

--- app/Http/Controllers/Auth/CustomAuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/CustomAuthenticatedSessionController.php
@@ -18,6 +18,7 @@ use Laravel\Fortify\Contracts\LogoutResponse;
 use Laravel\Fortify\Features;
 use Laravel\Fortify\Fortify;
 use Laravel\Fortify\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Hash;
 
 class CustomAuthenticatedSessionController extends Controller
 {
@@ -37,31 +38,35 @@ class CustomAuthenticatedSessionController extends Controller
     {
         // Verificar se o usuário existe
         $user = User::where('email', $request->email)->first();
-
+    
         if (!$user) {
             return back()->withErrors(['email' => 'Nenhum usuário encontrado.']);
         }
-
+    
         // Verificar se o usuário está ativo
         if (!$user->is_active) {
             return back()->withErrors(['email' => 'Usuário inativo. Entre em contato com o administrador.']);
         }
-
-        // Verificar se a senha é padrão (reset_password está true)
+    
+        // Verificar se a senha está correta
+        if (!Hash::check($request->password, $user->password)) {
+            return back()->withErrors(['password' => 'Senha incorreta.']);
+        }
+    
+        // Verificar se a senha é padrão (reset_password está false)
         if (!$user->reset_password) {
             // Armazenar o ID do usuário na sessão
             session(['password_reset_user_id' => $user->id]);
-
+    
             // Redirecionar para a página de redefinição de senha
             return redirect()->route('password.change.view')->with('status', 'Por favor, redefina sua senha antes de continuar.');
         }
-
+    
         // Continuar com o pipeline de autenticação
         return $this->loginPipeline($request)->then(function ($request) {
             return app(LoginResponse::class);
         });
     }
-
 
     protected function loginPipeline(LoginRequest $request)
     {
